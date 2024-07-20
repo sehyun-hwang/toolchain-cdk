272d84d40400352ca893d6b2b16dc98ca0388c25
Sehyun Hwang <hwanghyun3@gmail.com>
2024-07-20 15:43:35 +0900

    whoami API running on Spot instance EC2 on Bottlerocket OS

--- lib/ecs-playground-stack.ts
+++ lib/ecs-playground-stack.ts
@@ -1,16 +1,89 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+/* eslint-disable no-new */
+/* eslint-disable import/prefer-default-export */
 
-export class EcsPlaygroundStack extends cdk.Stack {
+// https://dev.to/aws-builders/autoscaling-using-spot-instances-with-aws-cdk-ts-4hgh
+
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import { AutoScalingGroup, HealthCheck } from 'aws-cdk-lib/aws-autoscaling';
+import {
+  InstanceArchitecture,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  Port,
+} from 'aws-cdk-lib/aws-ec2';
+import { ApplicationLoadBalancedEc2Service } from 'aws-cdk-lib/aws-ecs-patterns';
+import { ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { Construct } from 'constructs';
+import { Duration } from 'aws-cdk-lib';
+
+export default class HelloEcsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = ec2.Vpc.fromLookup(this, 'Vpc', {
+      isDefault: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsPlaygroundQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const autoScalingGroup = new AutoScalingGroup(this, 'AutoScalingGroup', {
+      vpc,
+      instanceType: InstanceType.of(
+        InstanceClass.BURSTABLE4_GRAVITON,
+        InstanceSize.MICRO,
+      ),
+      machineImage: new ecs.BottleRocketImage({
+        architecture: InstanceArchitecture.ARM_64,
+        variant: ecs.BottlerocketEcsVariant.AWS_ECS_2,
+      }),
+      allowAllOutbound: true,
+      maxCapacity: 3,
+      minCapacity: 1,
+      spotPrice: '0.007', // $0.0032 per Hour when writing, $0.0084 per Hour on-demand
+      healthCheck: HealthCheck.ec2(),
+    });
+
+    autoScalingGroup.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 50,
+      cooldown: Duration.minutes(1),
+      estimatedInstanceWarmup: Duration.minutes(1),
+    });
+
+    const asgCapacityProviderName = new cdk.CfnOutput(this, 'AsgCapacityProviderName', {
+      value: '',
+    });
+    const capacityProvider = new ecs.AsgCapacityProvider(this, 'AsgCapacityProvider', {
+      capacityProviderName: 'prefix-' + cdk.Names.nodeUniqueId(asgCapacityProviderName.node),
+      autoScalingGroup,
+      machineImageType: ecs.MachineImageType.BOTTLEROCKET,
+    });
+
+    const cluster = new ecs.Cluster(this, 'Cluster', {
+      vpc,
+    });
+    cluster.addAsgCapacityProvider(capacityProvider);
+
+    const certificate = Certificate.fromCertificateArn(
+      this,
+      'Certificate',
+      'arn:aws:acm:ap-northeast-1:248837585826:certificate/f6a51c7c-6e84-4b03-8f17-9dcce8b2d19a',
+    );
+
+    const loadBalancedEcsService = new ApplicationLoadBalancedEc2Service(this, 'Service', {
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('traefik/whoami'),
+        // containerPort: 9000,
+      },
+      cluster,
+      memoryLimitMiB: 512,
+      protocol: ApplicationProtocol.HTTPS,
+      // redirectHTTP: true,
+      certificate,
+    });
+
+    autoScalingGroup.connections.allowFrom(loadBalancedEcsService.loadBalancer, Port.allTcp());
+    loadBalancedEcsService.loadBalancer.connections.allowFrom(autoScalingGroup, Port.allTcp());
   }
 }
