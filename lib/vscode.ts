efdd19f3540e31a631083434e004765ef3974266
Sehyun Hwang <hwanghyun3@gmail.com>
2024-11-24 04:22:29 +0900

    Enable ssm agent

--- lib/vscode.ts
+++ lib/vscode.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
+import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 
 interface VsCodeEc2StackProps extends cdk.StackProps {
   vpc: ec2.IVpc;
@@ -32,5 +33,6 @@ export default class VsCodeEc2Stack extends cdk.Stack {
 
     const instance = new ec2.Instance(this, 'Instance', instanceProps);
     instance.connections.allowFromAnyIpv4(ec2.Port.tcp(22), 'SSH');
+    instance.role.addManagedPolicy(ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
   }
 }
