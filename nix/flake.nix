6827beda7a5db41be0b71590403ac1809eb07177
Sehyun Hwang <hwanghyun3@gmail.com>
2024-12-01 16:45:16 +0900

    alejandra formatting on nix files

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,16 +4,14 @@
     home-manager.url = "github:nix-community/home-manager/release-24.05";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-    }:
-    {
-      nixosConfigurations.ec2-dev = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [ "${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix" ];
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+  }: {
+    nixosConfigurations.ec2-dev = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = ["${nixpkgs}/nixos/modules/virtualisation/amazon-image.nix"];
     };
+  };
 }
